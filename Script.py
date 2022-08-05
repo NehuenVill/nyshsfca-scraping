4115b10d71d7d76bb8c7fc523844b881552175c9
JohnSolutions <jonhsmithd930@gmail.com>
2022-08-04 23:44:23 -0300

    Updated Script.py
    Finish the function for retrieving all the profile links from the website and tested it, also created a function using bs4 for retrieving all the info from each profile and tested it as well.

--- Script.py
+++ Script.py
@@ -1,27 +1,27 @@
-from base64 import encode
+from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
-import json
 from time import sleep
 
 
 # First get all the dynamicly load profiles href with selenium:
 
-driver = webdriver.Chrome()
 
-driver.get('http://www.nyshsfca.org/directory')
 
 def get_profiles_href():
 
+    driver = webdriver.Chrome()
+    driver.get('http://www.nyshsfca.org/directory')
+
     href_list = []
 
+    for i in range(27):
 
-    for i in range(19):
-
-        table = driver.find_element_by_tag_name('tbody')
+        table = driver.find_elements_by_tag_name('tbody')[2]
 
         hrefs = table.find_elements_by_tag_name('a')
 
-        sleep(3)
+        sleep(1)
 
         for href in hrefs:
 
@@ -31,25 +31,47 @@ def get_profiles_href():
 
             href_list.append(href_text)
 
-            sleep(0.1)
-
         selector = driver.find_element_by_tag_name('select')
 
         selector.click()
 
-        sleep(5)
+        sleep(3)
 
         options = selector.find_elements_by_tag_name('option')
 
         options[i+1].click()
 
-        sleep(2)
+        sleep(1)
     
     driver.close()
 
     return href_list
 
 
-get_profiles_href()
+# get_profiles_href()
+
 
 # Next use bs4 to get the information from those hrefs:
+
+def get_info(href_list : list):
+
+    for href in href_list:
+
+        r = requests.get(href)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        info = soup.find_all('div', class_ = 'fieldBody')
+
+        output = {
+            'Membership level': info[1].text.replace('\n', '').replace('/n', ''),
+            'First Name': info[2].text.replace('\n', '').replace('/n', ''),
+            'Last Name': info[3].text.replace('\n', '').replace('/n', ''),
+            'E-mail': info[4].text.replace('\n', '').replace('/n', ''),
+            'Phone': info[5].text.replace('\n', '').replace('/n', '')
+        }
+
+        print(output)
+
+        sleep(0.1)
+
+get_info(['http://www.nyshsfca.org/Sys/PublicProfile/63081055/3774357'])
