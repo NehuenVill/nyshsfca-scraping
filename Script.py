56be32ac4bbd2afe53ce300ec1d8ed33392d2101
JohnSolutions <jonhsmithd930@gmail.com>
2022-08-05 12:36:02 -0300

    Finished first version of the program
    Finished functions, added a feature to save the progress of URLs scraped in a JSON file and finnaly retrieved all available information into JSON and Excel files

--- Script.py
+++ Script.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from time import sleep
-
+import json
+import pandas as pd
 
 # First get all the dynamicly load profiles href with selenium:
 
@@ -15,21 +16,9 @@ def get_profiles_href():
 
     href_list = []
 
+    sleep(3)
+
     for i in range(27):
-
-        table = driver.find_elements_by_tag_name('tbody')[2]
-
-        hrefs = table.find_elements_by_tag_name('a')
-
-        sleep(1)
-
-        for href in hrefs:
-
-            href_text = href.get_attribute('href')
-
-            print(href_text)
-
-            href_list.append(href_text)
 
         selector = driver.find_element_by_tag_name('select')
 
@@ -39,10 +28,26 @@ def get_profiles_href():
 
         options = selector.find_elements_by_tag_name('option')
 
-        options[i+1].click()
+        options[i].click()
 
         sleep(1)
-    
+
+        table = driver.find_elements_by_tag_name('tbody')[2]
+
+        hrefs = table.find_elements_by_tag_name('a')
+
+        sleep(0.5)
+
+        for href in hrefs:
+
+            href_text = href.get_attribute('href')
+
+            print(href_text)
+
+            href_list.append(href_text)
+
+        sleep(0.5)
+
     driver.close()
 
     return href_list
@@ -55,7 +60,33 @@ def get_profiles_href():
 
 def get_info(href_list : list):
 
+    profiles = []
+
     for href in href_list:
+
+        # Saving the Urls the have been scraped already so that the program doesn't have
+        # to restart from zero each time it runs.
+
+        with open('sites_scraped.json') as f:
+
+            data = json.load(f)
+
+        with open('sites_scraped.json', 'w') as f:
+
+            if href not in data["Links"]:
+
+                data["Links"].append(href)
+
+                json.dump(data, f, indent=2)
+
+            else:
+
+                json.dump(data, f, indent=2)
+            
+                print("Already scraped")
+
+                pass
+
 
         r = requests.get(href)
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -63,15 +94,30 @@ def get_info(href_list : list):
         info = soup.find_all('div', class_ = 'fieldBody')
 
         output = {
-            'Membership level': info[1].text.replace('\n', '').replace('/n', ''),
             'First Name': info[2].text.replace('\n', '').replace('/n', ''),
             'Last Name': info[3].text.replace('\n', '').replace('/n', ''),
-            'E-mail': info[4].text.replace('\n', '').replace('/n', ''),
-            'Phone': info[5].text.replace('\n', '').replace('/n', '')
+            'Membership level': info[1].text.replace('\n', '').replace('/n', ''),
+            'E-mail': info[4].text.replace('\n', '').replace('/n', '') if len(info)>4 else 'Not available.',
+            'Phone': info[5].text.replace('\n', '').replace('/n', '') if len(info)>5 else 'Not available.'
         }
 
         print(output)
 
-        sleep(0.1)
+        profiles.append(output)
 
-get_info(['http://www.nyshsfca.org/Sys/PublicProfile/63081055/3774357'])
+    return profiles
+
+def save(data):
+
+    with open('High_school_couches.json', 'w') as f:
+
+        json.dump(data, f, indent=2)
+
+    df = pd.DataFrame(data, columns=['Membership level', 'First Name', 'Last Name', 'E-mail', 'Phone'])
+    df.to_excel('High_school_couches.xls', index=True, columns=['Membership level', 'First Name', 'Last Name', 'E-mail', 'Phone'])
+
+
+if __name__ == "__main__":
+
+    save(get_info(get_profiles_href()))
+
